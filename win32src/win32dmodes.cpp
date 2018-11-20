cbb2ac7aa67aba595b0cc5016dae906d2c28062a
forthe <37712025+EatTofuEveryDay@users.noreply.github.com>
2018-11-20 10:38:45 -0500

    Implement all

--- win32src/win32dmodes.cpp
+++ win32src/win32dmodes.cpp
@@ -21,18 +21,61 @@ void strokeWeight(double w){
   __private::staticvarlock.release();
 }
 
-void background(int, int, int, int){
-  
+void background(int r, int g, int b, int a = 0){
+  __private::staticvarlock.aquire();
+  double w = __private::width;
+  double h = __private::height;
+  __private::staticvarlock.release();
+  fill(r, g, b, a);
+  rect(0, 0, w, h);
 }
-void background(color_t);
-void fill(int, int, int, int);
-void fill(color_t);
-void noFill(void);
-void stroke(int, int, int, int);
-void stroke(color_t);
-void noStroke(void);
-color_t color(int, int, int, int);
-int alpha(color_t);
-int red(color_t);
-int green(color_t);
-int blue(color_t);
+void background(color_t){
+  __private::staticvarlock.aquire();
+  double w = __private::width;
+  double h = __private::height;
+  __private::staticvarlock.release();
+  fill(r, g, b, a);
+  rect(0, 0, w, h);
+}
+void fill(int r, int g, int b, int a = 0){
+  __private::staticvarlock.aquire();
+  // .... Might need to research how later
+  __private::staticvarlock.release();
+}
+void fill(color_t c){
+  fill(red(c), green(c), blue(c), alpha(c));
+}
+void noFill(void){
+  fill(0, 0, 0, 255);
+}
+void stroke(int r, int g, int b, int a = 0){
+  __private::staticvarlock.aquire();
+  // .... Might need to research how later
+  __private::staticvarlock.release();
+}
+void stroke(color_t c){
+  stroke(red(c), green(c), blue(c), alpha(c));
+}
+void noStroke(void){
+  stroke(0, 0, 0, 255);
+}
+color_t color(int r, int g, int b, int a = 0){
+  color_t ret;
+  ret.r = r;
+  ret.g = g;
+  ret.b = b;
+  ret.a = a;
+  return ret;
+}
+int alpha(color_t c){
+  return c.a;
+}
+int red(color_t c){
+  return c.r;
+}
+int green(color_t c){
+  return c.g;
+}
+int blue(color_t c){
+  return c.b;
+}
