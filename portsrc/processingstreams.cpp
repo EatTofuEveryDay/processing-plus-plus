9ddd428b96e9b4dbb8e14657923a736471f66c53
forthe <37712025+EatTofuEveryDay@users.noreply.github.com>
2018-11-20 20:09:04 -0500

    Update and rename processingstreams to processingstreams.cpp

--- portsrc/processingstreams.cpp
+++ portsrc/processingstreams.cpp
@@ -1,5 +1,7 @@
 #include "../include/processing.h"
 #include<iostream>
+#include<fstream>
+#include<string>
 
 std::ostream& operator<<(std::ostream& out, prxx::color_t c){
   out << "Color r: " << red(c) << ", g: " << green(c) << ", b: " << blue(c);
@@ -36,3 +38,38 @@ void prxx::print(std::string s){
   std::cout.flush();
 }
 
+prxx::file_t::file_t(){}
+
+prxx::file_t::file_t(std::string path, prxx::openmode o){
+  std::ios_base::openmode openm;
+  if(o & prxx::openmode::read)
+    openm |= std::ios::in;
+  if(o & prxx::openmode::write)
+    openm |= std::ios::out;
+  if(o & prxx::openmode::append)
+    openm |= std::ios::app;
+  if(o & prxx::openmode::trunc)
+    openm |= std::ios::trunc;
+  if(o & prxx::openmode::binary)
+    openm |= std::ios::binary;
+  fp = std::fstream(path, openm);
+}
+
+prxx::file_t::file_t(file_t f){
+  fp = f.fp;
+}
+
+std::string prxx::read(){
+  std::string buf;
+  std::string tmp;
+  while(fp.getline(tmp)){
+    buf += tmp;
+  }
+  return buf;
+}
+
+std::string prxx::read(unsigned int sz){
+  std::string buf;
+  fp.read(buf.c_str(), sz);
+  return buf;
+}
